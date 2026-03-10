8c7eb5be66e6594d0222a577b62fe46fdce549c5
st10056037 <Student@LabVM2049826>
2026-03-10 14:49:53 +0200

    i set up dbconetxt and added get connection string

---
+++ EventEase/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EventEase.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace EventEase.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public  ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Venue> Venues { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
